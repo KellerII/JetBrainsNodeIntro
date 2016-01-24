169827ba385c4d1f8dce6b5b1020c36c136b5f4f
James Thomas Keller II <jtkeller2@Jamess-MacBook-Pro.local>
2016-01-24 02:51:53 -0500

    Reformatted the code to meet IDE styling standards and added an asynchronous test.

--- tests/tests.js
+++ tests/tests.js
@@ -7,11 +7,27 @@ var should = require('should');
 var assert = require('assert');
 //var threeSeconds = require('seconds');
 
-describe ('Test Framework', function () {
-    it ('You should have mocha installed and running.', function () {
+describe('Test Framework', function () {
+    it('should have mocha installed and running.', function () {
         assert.equal(true, true);
     })
-    it ('You should have the should library installed and running for fluent testing.', function () {
+
+    it('should have the should library installed and running for fluent testing.', function () {
         true.should.eql(true)
+    })
+})
+
+describe('Asynchronous testing', function () {
+    var result = false;
+    beforeEach(function (done) {
+        result = true;
+        //complete the async beforeEach
+        done();
+
+    }, 2000);
+
+    it('should NOT be so hard so this makes it easier.', function (done) {
+        result.should.eql(true);
+        done();
     })
 })
